04e18810c04d458a948240e369268e8fb62492c6
Wangari-j <wangarijudy@gmail.com>
2024-03-20 23:21:20 +0300

    conditions

---
+++ conditions.py
@@ -0,0 +1,7 @@
+response = input("Would you like food? (Yes/No): ")
+
+if response == "Yes":  #for comparison we use double equals
+    print("Have some food!")
+else:
+    print("No food for you!")
+    
